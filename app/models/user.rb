b26e55de244e3a897f9dd90a41a4725bf3b4af44
Theeban Soundararajan <theeban@intersect.org.au>
2015-04-04 01:04:39 -0700

    update user spec with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  enum role: [:user, :vip, :admin]
+  enum role: [:user, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
@@ -9,5 +9,33 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
+
+  MAX_FIELD_LENGTH = 254
+
+  def self.max_name_length
+    MAX_FIELD_LENGTH
+  end
+
+  def self.max_email_length
+    MAX_FIELD_LENGTH
+  end
+
+  MIN_PASSWORD_LENGTH = 8
+  MAX_PASSWORD_LENGTH = 128
+
+  def self.min_password_length
+    MIN_PASSWORD_LENGTH
+  end
+
+  def self.max_password_length
+    MAX_PASSWORD_LENGTH
+  end
+
+  validates :name, presence: true, length: { maximum: max_name_length }
+  validates :email, presence: true, length: { maximum: max_email_length },
+    format: { with: /\A[a-zA-Z0-9_\.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+\z/ }
+  validates :password, presence: true, on: :create
+  validates :password, length: { minimum: min_password_length, maximum: max_password_length }, confirmation: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,96 @@
 describe User do
+  subject(:user) { User.new(name: name, email: email, password: password, password_confirmation: password_confirmation) }
 
-  before(:each) { @user = User.new(email: 'user@example.com') }
+  let(:name) { 'John Smith' }
+  let(:email) { 'john.smith@example.com' }
+  let(:password) { 'Pas$w0rd' }
+  let(:password_confirmation) { 'Pas$w0rd' }
 
-  subject { @user }
+  describe 'validations' do
+    before { user.valid? }
 
-  it { should respond_to(:email) }
+    it { expect(user).to be_valid }
 
-  it "#email returns a string" do
-    expect(@user.email).to match 'user@example.com'
+    describe '#name' do
+      context 'when blank' do
+        let(:name) { '' }
+        it { expect(user.errors[:name]).to include("can't be blank") }
+      end
+
+      context 'when equal to max name length' do
+        let(:name) { 'a' * User.max_name_length }
+        it { expect(user).to be_valid }
+      end
+
+      context 'when greater than max name length' do
+        let(:name) { 'a' * (User.max_name_length + 1) }
+        it { expect(user.errors[:name]).to eq(["is too long (maximum is #{User.max_name_length} characters)"]) }
+      end
+    end
+
+    describe '#email' do
+      context 'when blank' do
+        let(:email) { '' }
+        it { expect(user.errors[:email]).to include("can't be blank") }
+      end
+
+      context 'when equal to max email length' do
+        let(:email) { 'a@a.com' + 'a' * (User.max_email_length - 7) }
+        it { expect(user).to be_valid }
+      end
+
+      context 'when greater than max email length' do
+        let(:email) { 'a@a.com' + 'a' * (User.max_email_length - 7 + 1) }
+        it { expect(user.errors[:email]).to eq(["is too long (maximum is #{User.max_email_length} characters)"]) }
+      end
+
+      context 'when not valid' do
+        let(:email) { 'not an email' }
+        it { expect(user.errors[:email]).to include("is invalid") }
+      end
+    end
+
+    describe '#password' do
+      context 'when blank' do
+        let(:password) { '' }
+        let(:password_confirmation) { password }
+        it { expect(user.errors[:password]).to include("can't be blank") }
+      end
+
+      context 'when less than password min length' do
+        let(:password) { 'a' * (User.min_password_length - 1) }
+        let(:password_confirmation) { password }
+        it { expect(user.errors[:password]).to eq(["is too short (minimum is #{User.min_password_length} characters)"]) }
+      end
+
+      context 'when equal to password min length' do
+        let(:password) { 'a' * User.min_password_length }
+        let(:password_confirmation) { password }
+        it { expect(user).to be_valid }
+      end
+
+      context 'when equal to password max length' do
+        let(:password) { 'a' * User.max_password_length }
+        let(:password_confirmation) { password }
+        it { expect(user).to be_valid }
+      end
+
+      context 'when greater than password max length' do
+        let(:password) { 'a' * (User.max_password_length + 1) }
+        let(:password_confirmation) { password }
+        it { expect(user.errors[:password]).to eq(["is too long (maximum is #{User.max_password_length} characters)"]) }
+      end
+
+      # FIXME bug with validation for confirmation not working
+      #context 'when confirmation not equal' do
+        #let(:password) { 'Pas$w0rd' }
+        #let(:password_confirmation) { '' }
+        #it { expect(user.errors[:password]).to include("") }
+      #end
+    end
   end
 
+  describe '#role' do
+    it { expect(user.role).to eq('user') }
+  end
 end
