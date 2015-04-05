d9e1ed14c73591b82445c3be5c40d746c685a2e7
Theeban Soundararajan <theeban@intersect.org.au>
2015-04-04 22:25:07 -0700

    refactor sign up spec

--- spec/features/visitors/sign_up_spec.rb
+++ spec/features/visitors/sign_up_spec.rb
@@ -4,59 +4,66 @@
 #   So I can visit protected areas of the site
 feature 'Sign Up', :devise do
 
-  # Scenario: Visitor can sign up with valid email address and password
-  #   Given I am not signed in
-  #   When I sign up with a valid email address and password
-  #   Then I see a successful sign up message
-  scenario 'visitor can sign up with valid email address and password' do
-    sign_up_with('test@example.com', 'please123', 'please123')
-    txts = [I18n.t( 'devise.registrations.signed_up'), I18n.t( 'devise.registrations.signed_up_but_unconfirmed')]
-    expect(page).to have_content(/.*#{txts[0]}.*|.*#{txts[1]}.*/)
+  let(:name) { 'John Smith' }
+  let(:email) { 'john.smith@example.com' }
+  let(:password) { 'Pas$w0rd' }
+  let(:password_confirmation) { 'Pas$w0rd' }
+
+  before do
+    sign_up_with(name: name, email: email,
+                 password: password, password_confirmation: password_confirmation)
   end
 
-  # Scenario: Visitor cannot sign up with invalid email address
+  # Scenario: Visitor can sign up
+  #   Given I am not signed in
+  #   When I sign up with my details
+  #   Then I see a successful sign up message
+  scenario 'visitor can sign up' do
+    expect(page).to have_content(/#{I18n.t('devise.registrations.signed_up_but_unconfirmed')}/)
+  end
+
+  # Scenario: Visitor cannot sign up when name has errors
+  #   Given I am not signed in
+  #   When I sign up without a name
+  #   Then I see an 'name is blank' message
+  context 'with blank name' do
+    let(:name) { '' }
+    scenario 'visitor cannot sign up when name has errors' do
+      expect(page).to have_content "Name can't be blank"
+    end
+  end
+
+  # Scenario: Visitor cannot sign up when email address has errors
   #   Given I am not signed in
   #   When I sign up with an invalid email address
   #   Then I see an invalid email message
-  scenario 'visitor cannot sign up with invalid email address' do
-    sign_up_with('bogus', 'please123', 'please123')
-    expect(page).to have_content 'Email is invalid'
+  context 'with invalid email address' do
+    let(:email) { 'bogus' }
+    scenario 'visitor cannot sign up when email address has errors' do
+      expect(page).to have_content 'Email is invalid'
+    end
   end
 
-  # Scenario: Visitor cannot sign up without password
+  # Scenario: Visitor cannot sign up when password has errors
   #   Given I am not signed in
   #   When I sign up without a password
   #   Then I see a missing password message
-  scenario 'visitor cannot sign up without password' do
-    sign_up_with('test@example.com', '', '')
-    expect(page).to have_content "Password can't be blank"
+  context 'with blank password' do
+    let(:password) { '' }
+    scenario 'visitor cannot sign up when password has errors' do
+      expect(page).to have_content "Password can't be blank"
+    end
   end
 
-  # Scenario: Visitor cannot sign up with a short password
-  #   Given I am not signed in
-  #   When I sign up with a short password
-  #   Then I see a 'too short password' message
-  scenario 'visitor cannot sign up with a short password' do
-    sign_up_with('test@example.com', 'please', 'please')
-    expect(page).to have_content "Password is too short"
-  end
-
-  # Scenario: Visitor cannot sign up without password confirmation
+  # Scenario: Visitor cannot sign up when password confirmation has errors
   #   Given I am not signed in
   #   When I sign up without a password confirmation
   #   Then I see a missing password confirmation message
-  scenario 'visitor cannot sign up without password confirmation' do
-    sign_up_with('test@example.com', 'please123', '')
-    expect(page).to have_content "Password confirmation doesn't match"
-  end
-
-  # Scenario: Visitor cannot sign up with mismatched password and confirmation
-  #   Given I am not signed in
-  #   When I sign up with a mismatched password confirmation
-  #   Then I should see a mismatched password message
-  scenario 'visitor cannot sign up with mismatched password and confirmation' do
-    sign_up_with('test@example.com', 'please123', 'mismatch')
-    expect(page).to have_content "Password confirmation doesn't match"
+  context 'with blank password confirmation' do
+    let(:password_confirmation) { '' }
+    scenario 'visitor cannot sign up when password confirmation has errors' do
+      expect(page).to have_content "Password confirmation doesn't match"
+    end
   end
 
 end
