52843d6535fe63e6e9b7a45c56bd7d8f5a174f42
Theeban Soundararajan <theeban@intersect.org.au>
2015-04-04 22:41:28 -0700

    refactor sign out spec

--- spec/features/users/sign_out_spec.rb
+++ spec/features/users/sign_out_spec.rb
@@ -4,14 +4,17 @@
 #   So I can protect my account from unauthorized access
 feature 'Sign out', :devise do
 
+  before do
+    user = FactoryGirl.create(:user)
+    sign_in(user.email, user.password)
+    expect(page).to have_content I18n.t 'devise.sessions.signed_in'
+  end
+
   # Scenario: User signs out successfully
   #   Given I am signed in
   #   When I sign out
   #   Then I see a signed out message
   scenario 'user signs out successfully' do
-    user = FactoryGirl.create(:user)
-    sign_in(user.email, user.password)
-    expect(page).to have_content I18n.t 'devise.sessions.signed_in'
     click_link 'Sign out'
     expect(page).to have_content I18n.t 'devise.sessions.signed_out'
   end
