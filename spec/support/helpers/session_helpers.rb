caa1cf480c12ef69962a560897775db9096a7ac1
Theeban Soundararajan <theeban@intersect.org.au>
2015-04-05 16:39:36 -0700

    fix sign up button to use get + fix broken feature tests

--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -2,18 +2,22 @@ module Features
   module SessionHelpers
     def sign_up_with(args)
       visit new_user_registration_path
-      fill_in 'Name', with: args[:name]
-      fill_in 'Email', with: args[:email]
-      fill_in 'Password', with: args[:password]
-      fill_in 'Password confirmation', with: args[:password_confirmation]
-      click_button 'Sign up'
+      within '.authform' do
+        fill_in 'Name', with: args[:name]
+        fill_in 'Email', with: args[:email]
+        fill_in 'Password', with: args[:password]
+        fill_in 'Password confirmation', with: args[:password_confirmation]
+        click_button 'Sign up'
+      end
     end
 
     def sign_in(email, password)
       visit new_user_session_path
-      fill_in 'Email', with: email
-      fill_in 'Password', with: password
-      click_button 'Sign in'
+      within '.authform' do
+        fill_in 'Email', with: email
+        fill_in 'Password', with: password
+        click_button 'Sign in'
+      end
     end
   end
 end
