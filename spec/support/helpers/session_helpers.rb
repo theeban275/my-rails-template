99b122f41c15f56d56fb2c122d601bec2fa5a7ea
Theeban Soundararajan <theeban@intersect.org.au>
2015-04-04 22:25:32 -0700

    add file missing from last commit

--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -1,10 +1,11 @@
 module Features
   module SessionHelpers
-    def sign_up_with(email, password, confirmation)
+    def sign_up_with(args)
       visit new_user_registration_path
-      fill_in 'Email', with: email
-      fill_in 'Password', with: password
-      fill_in 'Password confirmation', :with => confirmation
+      fill_in 'Name', with: args[:name]
+      fill_in 'Email', with: args[:email]
+      fill_in 'Password', with: args[:password]
+      fill_in 'Password confirmation', with: args[:password_confirmation]
       click_button 'Sign up'
     end
 
