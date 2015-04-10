d9207aa540a7dd69e42ac7c5866c666b5278024d
Theeban Soundararajan <theeban@intersect.org.au>
2015-04-10 21:02:17 +1000

    update database schema to support postgresql + move users index spec to admins list spec

--- spec/features/admins/users_index_spec.rb
+++ spec/features/admins/users_index_spec.rb
@@ -1,23 +1,19 @@
-include Warden::Test::Helpers
-Warden.test_mode!
-
-# Feature: User index page
-#   As a user
+# Feature: List users
+#   As a admin
 #   I want to see a list of users
 #   So I can see who has registered
 feature 'User index page', :devise do
+  let(:user) { FactoryGirl.create(:user, :admin) }
 
-  after(:each) do
-    Warden.test_reset!
+  before do
+    sign_in(user.email, user.password)
   end
 
   # Scenario: User listed on index page
   #   Given I am signed in
   #   When I visit the user index page
-  #   Then I see my own email address
+  #   Then I should see a list of users
   scenario 'user sees own email address' do
-    user = FactoryGirl.create(:user, :admin)
-    login_as(user, scope: :user)
     visit users_path
     expect(page).to have_content user.email
   end
