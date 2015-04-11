80b4c61e487802631541c71f23c0146b16c33069
Theeban Soundararajan <theeban@intersect.org.au>
2015-04-11 13:37:40 +1000

    fix bug with seed failing to create admin user

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,6 +1,7 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
+    secrets = Rails.application.secrets
+    user = User.find_or_create_by!(name: secrets.admin_name, email: secrets.admin_email) do |user|
         user.password = Rails.application.secrets.admin_password
         user.password_confirmation = Rails.application.secrets.admin_password
         user.confirm!
